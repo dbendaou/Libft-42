b814249374b873c24ec088639aff95999af2dc5f
dbendaou <dbendaou@student.42.fr>
2016-02-02 16:39:26 +0100

    mise aux normes

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -22,7 +22,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	if ((str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+	if ((str = (char *)malloc(sizeof(char) *
+		(ft_strlen(s1) + ft_strlen(s2) + 1))))
 	{
 		while (s1[i])
 		{
